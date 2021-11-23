a1491dfd8a100c7fe38d723fce56b797641e8a8b
leinys <lepagifo@hotmail.com>
2021-11-23 11:05:49 -0500

    leinys

--- appfronte/src/app/app-routing.module.ts
+++ appfronte/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { CreateClienteComponent } from './components/cliente/create-cliente/crea
 import { ShowClienteComponent } from './components/cliente/show-cliente/show-cliente.component';
 import { CreateProductoComponent } from './components/producto/create-producto/create-producto.component';
 import { ShowProductoComponent } from './components/producto/show-producto/show-producto.component';
+import { CreateProveedorComponent } from './components/proveedor/create-proveedor/create-proveedor.component';
+import { ShowProveedorComponent } from './components/proveedor/show-proveedor/show-proveedor.component';
 
 const routes: Routes = [{
   path: "producto",
@@ -20,7 +22,20 @@ const routes: Routes = [{
   {
 		path: "crearUsers",
 		component: CreateClienteComponent
-	}
+	},
+
+  {
+   
+    path: "proveedor",
+    component: ShowProveedorComponent
+  
+  
+  },
+     {
+  
+      path: "crearproveedor",
+      component: CreateProveedorComponent
+     },
     
 ];
 
