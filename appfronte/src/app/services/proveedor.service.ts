5d5279b2fc6d26a71e6befcf1ddbd687ea4ec7c5
leinys <lepagifo@hotmail.com>
2021-11-23 11:11:26 -0500

    leinys

---
+++ appfronte/src/app/services/proveedor.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProveedorI } from '../models/ProveedorI';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProveedorService {
+
+  constructor() { }
+}
