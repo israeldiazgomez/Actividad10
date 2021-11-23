eda7b4d3924de0f00536a30f8202e1f2aa77c3a8
leinys <lepagifo@hotmail.com>
2021-11-23 11:22:19 -0500

    leinys

--- appfronte/src/app/components/proveedor/show-proveedor/show-proveedor.component.ts
+++ appfronte/src/app/components/proveedor/show-proveedor/show-proveedor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProveedorI } from 'src/app/models/ProveedorI';
+import { ProveedorService } from 'src/app/services/proveedor.service';
 
 @Component({
   selector: 'app-show-proveedor',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-proveedor.component.css']
 })
 export class ShowProveedorComponent implements OnInit {
+  public proveedores: ProveedorI[] = []
+  public displayedColumns: string[] = ["id", "nombre", "apellido", "direccion", "provincia", "telefono"]
 
-  constructor() { }
+  constructor(
+    private proveedorService: ProveedorService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.mostrarProveedor();
   }
+  mostrarProveedor() {
+    this.proveedorService.getAll().subscribe({
+      next: (data) => {
+        this.proveedores = data
+        console.log(data)
+      }
+    })
+  }
+  
 
 }
