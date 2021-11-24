c700d18fd35655a38f2603e478fe3a79d353aee9
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2021-11-23 22:34:25 -0500

    marlon

---
+++ appfronte/src/app/components/index/show-index/show-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-index',
+  templateUrl: './show-index.component.html',
+  styleUrls: ['./show-index.component.css']
+})
+export class ShowIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
